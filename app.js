437653719f37a67cdb3e9033c47701a5a2d9820d
Emil <emil.emanuel@outlook.com>
2019-08-15 20:06:00 +0200

    fixed app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
 import helmet from 'helmet';
+import usersRouter from './routes/userroute';
 
 import config from './config/config';
 
@@ -13,18 +14,18 @@ const APP = express();
 
 // connect to mongodb ASAP
 mongoose
-  .connect( config.db.connectionString, { useNewUrlParser: true } )
-  .then( () => console.log( 'Successfully connected to database.' ) )
-  .catch( error => console.log( error ) );
+  .connect(config.db.connectionString, { useNewUrlParser: true })
+  .then(() => console.log('Successfully connected to database.'))
+  .catch(error => console.log(error));
 
-APP.use( helmet );
-APP.use( logger( 'dev' ) );
-APP.use( express.json() );
-APP.use( express.urlencoded( { extended: false } ) );
-APP.use( cookieParser() );
-APP.use( express.static( path.join( __dirname, 'public' ) ) );
+APP.use(helmet());
+APP.use(logger('dev'));
+APP.use(express.json());
+APP.use(express.urlencoded({ extended: false }));
+APP.use(cookieParser());
+APP.use(express.static(path.join(__dirname, 'public')));
 
-APP.use( '/', ROUTER );
-// app.use('/users', usersRouter);
+APP.use('/', ROUTER);
+APP.use('/users', usersRouter);
 
 export default APP;
