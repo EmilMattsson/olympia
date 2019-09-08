5d1b22b07d527899843e3591d32f85dfe1d543b3
EmilMattsson <emileo1992@gmail.com>
2019-09-08 17:18:19 +0200

    refactored email and password validator

--- middleware/validator.js
+++ middleware/validator.js
@@ -4,14 +4,23 @@ export function validateUserEmailAndPassword(req, res, next) {
   const UNVERIFIED_USER_EMAIL = req.body.email;
   const UNVERIFIED_USER_PASSWORD = req.body.password;
   
-  if (UNVERIFIED_USER_EMAIL == undefined || !validator.isEmail(UNVERIFIED_USER_EMAIL)) {
-    console.error('EMAIL_REQUIRED');
-  } else if (UNVERIFIED_USER_PASSWORD == undefined || UNVERIFIED_USER_PASSWORD == '') {
-    console.error('PASSWORD_REQUIRED');
-  } else {
-    next();
-  }
+  validateEmail(UNVERIFIED_USER_EMAIL);
+  validatePassword(UNVERIFIED_USER_PASSWORD);
+
+  next();
 }
+
+const validateEmail = email => {
+  if (email === undefined || !validator.isEmail(email)) {
+    throw new Error('EMAIL_REQUIRED');
+  }
+};
+
+const validatePassword = password => {
+  if (password === undefined || validator.isEmpty(password)) {
+    throw new Error('PASSWORD_REQUIRED');
+  }
+};
 
 const isAlphabetical = str => {
   return /^[A-Za-z]+$/.test(str);
